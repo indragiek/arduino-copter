7091beebf0ab94fe36a1b8651fe9ded218a86c47
Indragie Karunaratne <i@indragie.com>
2013-11-26 09:53:18 -0700

    Add BLE framework, implement UI controls

--- ios/CopterControl/CopterControl/CPTViewController.m
+++ ios/CopterControl/CopterControl/CPTViewController.m
@@ -8,8 +8,17 @@
 
 #import "CPTViewController.h"
 
-@interface CPTViewController ()
+#define CPTViewBackgroundColor	[UIColor colorWithRed:0.14 green:0.14 blue:0.16 alpha:1.0]
+#define CPTViewScoreFont		[UIFont fontWithName:@"DBLCDTempBlack" size:24.f]
+#define CPTViewScoreTextColor	[UIColor colorWithRed:0.27 green:1.00 blue:0.08 alpha:1.0]
+#define CPTViewHiScoreTextColor	[UIColor colorWithRed:0.24 green:0.58 blue:0.88 alpha:1.0]
 
+@interface CPTViewController ()
+@property (nonatomic, weak) IBOutlet UILabel *scoreLabel;
+@property (nonatomic, weak) IBOutlet UILabel *highScoreLabel;
+- (IBAction)buttonDown:(id)sender;
+- (IBAction)buttonUp:(id)sender;
+- (IBAction)playPause:(id)sender;
 @end
 
 @implementation CPTViewController
@@ -17,13 +26,32 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.view.backgroundColor = CPTViewBackgroundColor;
+	self.scoreLabel.font = CPTViewScoreFont;
+	self.highScoreLabel.font = CPTViewScoreFont;
+	self.scoreLabel.textColor = CPTViewScoreTextColor;
+	self.highScoreLabel.textColor = CPTViewHiScoreTextColor;
+	
+	self.scoreLabel.text = @"1026";
+	self.highScoreLabel.text = @"11230";
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Actions
+
+- (IBAction)buttonDown:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	NSLog(@"Button down");
+}
+
+- (IBAction)buttonUp:(id)sender
+{
+	NSLog(@"Button up");
+}
+
+- (IBAction)playPause:(UIButton *)sender
+{
+	sender.selected = !sender.selected;
+	NSLog(@"Play pause");
 }
 
 @end
