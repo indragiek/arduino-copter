2991178079dcec90e3feb0aa2fbdceedf8adeee9
Indragie Karunaratne <i@indragie.com>
2013-11-30 15:35:42 -0700

    Cleanup bt_transmitter

--- arduino/bt_transmitter/bt_transmitter.cpp
+++ arduino/bt_transmitter/bt_transmitter.cpp
@@ -3,31 +3,9 @@
 //
 // Created November 21, 2013
 //
-// Game controls and score transfer over Bluetooth.
-// This program runs on a second Arduino and handles Bluetooth communication.
+// Does Bluetooth I/O over Serial port.
 
 #include <ble_shield.h>
-
-// ======== Specification ========
-//
-// This specification defines the communication protocol used by copter to
-// exchange data between the game and an external controller device. The
-// following commands are implemented:
-//
-// 1) RECEIVE: Button Press Down
-//    Byte sequence: 0x01 0x0`
-//
-// 2) RECEIVE: Button Press Up
-//    Byte sequence: 0x01 0x00
-//
-// 3) RECEIVE: Toggle play/pause
-//    Byte sequence: 0x02
-//
-// 4) SEND: Game reset signal.
-//    Byte sequence: 0x03
-//
-// 3) SEND: Increment score by 1pt.
-//    Byte sequence: 0x04
 
 void setup() {
 	Serial.begin(9600);
@@ -38,13 +16,10 @@ void loop() {
 	ble_do_events();
 	if (!ble_connected()) return;
 
-	int num_bytes = ble_available();
-	for (int i = 0; i < num_bytes; i++) {
+	while (ble_available()) {
 		Serial.write(ble_read());
 	}
-
-	num_bytes = Serial.available();
-	for (int i = 0; i < num_bytes; i++) {
+	while (Serial.available()) {
 		ble_write(Serial.read());
 	}
 }
