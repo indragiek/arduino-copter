b5e541fd09a010a3524e5f038a0e3c84fc034d75
Indragie Karunaratne <i@indragie.com>
2013-12-02 23:14:34 -0700

    Little bit more optimized drawing

--- arduino/copter/drawing_utils.cpp
+++ arduino/copter/drawing_utils.cpp
@@ -7,7 +7,23 @@
 #include "drawing_utils.h"
 
 void draw_rect(Adafruit_GFX *tft, g_rect rect, int color) {
-	tft->fillRect(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height, color);
+	const int x = rect.origin.x;
+	const int y = rect.origin.y;
+	const int w = rect.size.width;
+	const int h = rect.size.height;
+
+	boolean w_unit = w == 1;
+	boolean h_unit = h == 1;
+
+	if (w_unit == true && h_unit == true) {
+		draw_pixel(tft, rect.origin, color);
+	} else if (w_unit == true) {
+		tft->drawFastVLine(x, y, h, color);
+	} else if (h_unit == true) {
+		tft->drawFastHLine(x, y, w, color);
+	} else {
+		tft->fillRect(x, y, w, h, color);
+	}
 }
 
 void draw_pixel(Adafruit_GFX *tft, g_point point, int color) {
